8260b6506178ad0c685282ff0581699434b6cbe8
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-01-22 12:33:54 +0530

    Create assignment9_Q3.py

---
+++ Assignment-9/assignment9_Q3.py
@@ -0,0 +1,11 @@
+def Square(c):
+    c = c*c
+    return c
+
+def main():
+    No = int(input("Enter the number: "))
+    Result = Square(No)
+    print("The Square is: ",Result)
+    
+if __name__ == "__main__":
+    main()
