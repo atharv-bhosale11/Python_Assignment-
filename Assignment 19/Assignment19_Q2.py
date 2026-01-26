291466dcafc9acb6619b452791f740f4af71c64e
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-01-26 19:24:49 +0530

    Create Assignment19_Q2.py

---
+++ Assignment 19/Assignment19_Q2.py
@@ -0,0 +1,10 @@
+Multiplication = lambda a,b : a*b
+
+def main():
+        Num1 = int(input("Enter the Number: "))
+        Num2 = int(input("Enter the Number: "))
+        Ret = Multiplication(Num1,Num2)
+        print("Multiplication is: ",Ret)
+
+if __name__=="__main__":
+        main()
