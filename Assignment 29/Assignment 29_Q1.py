f8aafb237b739e3c2cc4d308878421f3fc7bb62c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-02-02 20:19:01 +0530

    Update Assignment 29_Q1.py

--- Assignment 29/Assignment 29_Q1.py
+++ Assignment 29/Assignment 29_Q1.py
@@ -1,15 +1,17 @@
 import os
 
-def main():
-    FileName = input("Enter the File Name: ")
-
+def CheckFile(FileName):
     Ret = os.path.exists(FileName)
     
     if Ret == True:
         print("File Found Successfully!!!!!")
     else:
         print("There is No such File :/")
+    return Ret
 
-
+def main():
+    FileName = input("Enter the File Name: ")
+    CheckFile(FileName)
+    
 if __name__=="__main__":
     main()
