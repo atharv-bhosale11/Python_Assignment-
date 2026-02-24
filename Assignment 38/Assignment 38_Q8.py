821afad6175b8dfb9ff89e26aedbaecae551e1ca
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-02-25 00:07:40 +0530

    Create Assignment 38_Q8.py

---
+++ Assignment 38/Assignment 38_Q8.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.boxplot(data=df,x="Attendance")
+plt.title("Student Performance ML")
+plt.show()
