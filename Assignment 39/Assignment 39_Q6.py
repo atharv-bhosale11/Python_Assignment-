6f91664f9c12810f5f0fddc31d535c8fb1d31f3b
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-02-25 20:59:42 +0530

    Create Assignment 39_Q6.py

---
+++ Assignment 39/Assignment 39_Q6.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    ConfusionMatrixDisplay
+)
+
+feature_cols=[
+    "StudyHours",
+    "Attendance",
+    "PreviousScore",
+    "AssignmentsCompleted",
+    "SleepHours"
+]
+
+X = df[feature_cols]        #Independent variables
+Y = df["FinalResult"]       #dependent variables
+
+print("X Shape : ",X.shape)
+print("Y Shape : ",Y.shape)
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y,
+    test_size=0.4,       # 0.2 means 20%
+    random_state=42      # Shuffle the Data
+)
+
+print("Data Splitting Activity Done!!!!!")
+
+print("X - Independent :",X.shape)  
+print("Y - Dependent :",Y.shape)   
+
+print("X_train :",X_train.shape)   
+print("X_test :",X_test.shape)      
+
+print("Y_train :",Y_train.shape)    
+print("Y_test :",Y_test.shape)      
+
+
+print(" I am using DecisionTreeClassifier") 
+
+#Actual Model Implementation
+
+model = DecisionTreeClassifier(         
+    criterion="gini",
+    max_depth=3,            #hyper parameter tuning
+    random_state=42
+)
+
+print("Model Successfully Created: ",model)
+
+model.fit(X_train,Y_train)
+
+print("Model Training Completed!!!!!")
