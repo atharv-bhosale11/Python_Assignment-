564cf358cf848599eeeb22e9da34571beda41e78
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-03-13 17:32:09 +0530

    Create Assignment 48_Q1.py

---
+++ Assignment 48/Assignment 48_Q1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def Mean():
+
+    data = [6,7,8,9,10,11,12]    #Load the Data
+
+    Mean = np.mean(data)
+
+    print("Mean: ",Mean)
+
+
+def main():
+    Mean()
+
+if __name__ =="__main__":
+    main()
